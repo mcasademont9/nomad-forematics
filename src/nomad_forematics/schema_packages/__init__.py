a466ca6733313d0fca300c1209fc29f1c73a6ac1
Miquel Casademont <mcasademont9@github.com>
2025-03-27 11:53:29 +0100

    Ruff linting some errors

--- src/nomad_forematics/schema_packages/__init__.py
+++ src/nomad_forematics/schema_packages/__init__.py
@@ -1,26 +1,25 @@
 from nomad.config.models.plugins import SchemaPackageEntryPoint
 
+# class NewSchemaPackageEntryPoint(SchemaPackageEntryPoint):
+#     parameter: int = Field(0, description='Custom configuration parameter')
 
-class NewSchemaPackageEntryPoint(SchemaPackageEntryPoint):
-    parameter: int = Field(0, description='Custom configuration parameter')
+#     def load(self):
+#         from nomad_forematics.schema_packages.schema_package import m_package
 
-    def load(self):
-        from nomad_forematics.schema_packages.schema_package import m_package
-
-        return m_package
+#         return m_package
 
 
-schema_package_entry_point = NewSchemaPackageEntryPoint(
-    name='NewSchemaPackage',
-    description='New schema package entry point configuration.',
-)
+# schema_package_entry_point = NewSchemaPackageEntryPoint(
+#     name='NewSchemaPackage',
+#     description='New schema package entry point configuration.',
+# )
 
-class SubstrateEntryPoint(SchemaPackageEntryPoint)
+class SubstrateEntryPoint(SchemaPackageEntryPoint):
     def load(self):
         from nomad_forematics.schema_packages.substrate import m_package
 
         return m_package
-        
+
 substrate = SubstrateEntryPoint(
     name='Substrate',
     description='Schema package defined for substrate.',
